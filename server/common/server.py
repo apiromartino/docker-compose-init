7153cd6c1b47c8a00e7cd00dc5d7aa009e6b3647
Ariel <ariel.piro@creativecoefficient.net>
2022-04-03 16:00:16 -0300

    Saving clients sockets and processes to exit gracefully

--- server/common/server.py
+++ server/common/server.py
@@ -1,3 +1,4 @@
+from signal import signal
 import socket
 import logging
 import multiprocessing
@@ -10,29 +11,35 @@ class Server:
         self._server_socket.bind(('', port))
         self._server_socket.listen(listen_backlog)
         self.listen_backlog = listen_backlog
+        self.processes = []
+        self.client_sockets = {}
 
     def run(self):
-        """
-        Dummy Server loop
 
-        Server that accept a new connections and establishes a
-        communication with a client. After client with communucation
-        finishes, servers starts to accept new connections again
-        """
+        signal.signal(signal.SIGTERM, self.exit_gracefully)
 
         processes_number = self.listen_backlog if self.listen_backlog <= multiprocessing.cpu_count(
         ) else multiprocessing.multiprocessing.cpu_count()
-        for _ in range(0, processes_number):
+        for i in range(0, processes_number):
             p = multiprocessing.Process(
-                target=self.__new_connection)
+                target=self.__new_connection, args=(i,))
             p.start()
+            self.processes.append(p)
 
-    def __new_connection(self):
+    def __new_connection(self, client_id):
+        """
+        Dummy Process Server loop
+
+        Server that accept a new connections and establishes a
+        communication with a client. After client with communication
+        finishes, this process starts to accept new connections again
+        """
         while True:
             client_sock = self.__accept_new_connection()
-            self.__handle_client_connection(client_sock)
+            self.client_sockets[client_id] = client_sock
+            self.__handle_client_connection(client_sock, client_id)
 
-    def __handle_client_connection(self, client_sock):
+    def __handle_client_connection(self, client_sock, client_id):
         """
         Read message from a specific client socket and closes the socket
 
@@ -50,6 +57,7 @@ class Server:
             logging.info("Error while reading socket {}".format(client_sock))
         finally:
             client_sock.close()
+            del self.client_sockets[client_id]
 
     def __accept_new_connection(self):
         """
@@ -64,3 +72,6 @@ class Server:
         c, addr = self._server_socket.accept()
         logging.info('Got connection from {}'.format(addr))
         return c
+
+    def exit_gracefully(self, sig, frame):
+        return
